a6119759a0aaa206b73a3e02fc3166fadd9d6117
Onkar Tarar <tararonkar@protonmail.com>
2024-10-03 13:02:57 +0530

    Auth and data removed

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,31 @@ import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { Authenticator } from "@aws-amplify/ui-react";
 
-const client = generateClient<Schema>();
+// const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+//   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
+//   useEffect(() => {
+//     client.models.Todo.observeQuery().subscribe({
+//       next: (data) => setTodos([...data.items]),
+//     });
+//   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+//   function createTodo() {
+//     client.models.Todo.create({ content: window.prompt("Todo content") });
+//   }
 
   return (
     <Authenticator>
         <main>
         <h1>My todos</h1>
-        <button onClick={createTodo}>+ new</button>
+        {/* <button onClick={createTodo}>+ new</button>
         <ul>
             {todos.map((todo) => (
             <li key={todo.id}>{todo.content}</li>
             ))}
-        </ul>
+        </ul> */}
         <div>
             🥳 App successfully hosted. Try creating a new todo.
             <br />
